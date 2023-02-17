bea1d9f4cf2ae2f6d24ed1dc9992df853e315c97
Prime <pchikweru@gmail.com>
2023-02-16 20:11:13 -0800

    using gitacp

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
