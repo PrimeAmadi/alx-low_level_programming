054ea6338d5aee2525fc65038f5b309e06678d64
Prime <pchikweru@gmail.com>
2023-02-16 19:43:58 -0800

    using gitacp

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+	return (0);
+}
