73361d1d5ed4e1c8485aa4c4d3101474183e5a0c
Prime <pchikweru@gmail.com>
2023-02-23 19:58:45 -0800

    using gitacp

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - takes an integer and evaluatas
+ * if it is the ASCII value of an upper case
+ * letter
+ * @c: the int value to evaluate
+ * Return: 1 for true it is an uppercase letter,
+ * and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if( c > 64 && c < 91)
+		return (1);
+	return(0);
+}
