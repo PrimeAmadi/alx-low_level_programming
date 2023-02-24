c581644bb42295b59aa3e8bc6d732cf4b7547bb2
Prime <pchikweru@gmail.com>
2023-02-23 20:20:43 -0800

    using gitacp

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - check if a character 
+ * represents a digit
+ * @c: the int value of the char to
+ * be eval
+ * Return: 1 if @c is digit,
+ * 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if(c > 46 && c < 58)
+		return (1);
+	return (0);
+}
