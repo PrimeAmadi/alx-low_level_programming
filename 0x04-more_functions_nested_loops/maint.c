008ed193a3d2efb9d361b88761a8c72fd0f708e0
Prime <pchikweru@gmail.com>
2023-02-24 16:32:01 -0800

    using gitacp

---
+++ 0x04-more_functions_nested_loops/maint.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - for testing
+ * Return: 0
+ */
+
+int main(void)
+{
+	if (_isdigit('4'))
+		printf("is digit\n");
+	else
+		printf("not digit\n");
+	return (0);
+}
