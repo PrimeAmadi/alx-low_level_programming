e1641ec78b89302236e726f926fcdebcd89c0957
Prime <pchikweru@gmail.com>
2023-02-24 17:58:02 -0800

    using gitacp

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print to 14
+ * 10 times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int d;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (d = 0; d <= 14; d++)
+		{
+			if (d < 10)
+				putchar('0' + d);
+			if (d >= 10)
+			{
+				putchar('1');
+				putchar('0' + d - 10);
+			}
+		}
+		putchar('\n');
+	}
+
+	/*putchar('\n');*/
+}
