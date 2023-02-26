1c8f74abe2e05bbaf38400850a849baeba9fbe0e
Prime <pchikweru@gmail.com>
2023-02-25 20:15:22 -0800

    using gitacp

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,33 @@
-#include <stdio.h>
-
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int num1, num2;
+	int num1 = 0, num2 = 0;
 
-	for (num1 = 0; num1 < 100; num1++)
+	while (num1 <= 99)
 	{
-		for (num2 = 0; num2 < 100; num2++)
+		num2 = num1 + 1;
+		while (num2 <= 99)
 		{
-			if (num1 <= num2)
+			putchar(num1 / 10 + '0');
+			putchar(num1 % 10 + '0');
+			putchar(' ');
+			putchar(num2 / 10 + '0');
+			putchar(num2 % 10 + '0');
+			if (!(num1 == 98 && num2 == 99))
 			{
-				putchar(num1 / 10 + '0');
-				putchar(num1 % 10 + '0');
+				putchar(',');
 				putchar(' ');
-				putchar(num2 / 10 + '0');
-				putchar(num2 % 10 + '0');
-
-				if (num1 != 98 || num2 != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
+			num2++;
 		}
+		num1++;
 	}
-
-	putchar('\n'); /* Add a newline at the end of the output */
+	putchar('\n');
 
 	return (0);
 }
