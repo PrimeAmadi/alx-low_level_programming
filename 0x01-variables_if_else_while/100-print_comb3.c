53767b82a5d37eac8254f3dbf427254e705681be
Prime <pchikweru@gmail.com>
2023-02-25 19:13:35 -0800

    using gitacp

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print every combo
+ * of 2 digit, non-repeating
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{	/*Add ", " after number except the last one*/
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
