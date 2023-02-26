44df6209607b0af6bfd8f1656ae467c7ffc6bb8a
Prime <pchikweru@gmail.com>
2023-02-26 15:32:04 -0800

    using gitacp

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-
-:";
-
-	puts(str);
-	return (0);
+int main(void)
+{
+	fprintf(stderr, "\"and that piece of art is useful\" "
+		"- Dora Korpar, 2015-10-19\n");
+	return 0;
 }
+
