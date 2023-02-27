bf62c3d757a49835b78fe538f3284d239c277f4d
Prime <pchikweru@gmail.com>
2023-02-26 19:18:34 -0800

    using gitacp

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+/**
+ * print_line - print line on
+ * the console
+ * @n: the length of the line
+ * in '_'
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+		while (n--)
+		{
+			putchar('-');
+		}
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/maint.c
+++ 0x04-more_functions_nested_loops/maint.c
@@ -11,7 +11,10 @@ int main(void)
 //	else
 //		printf("not digit\n");
 //	print_most_numbers();
-	more_numbers();
+//	more_numbers();
+	for(int i = 0; i < 10; i++)
+		print_line(i);
+
 //	printf("\n");
 
 	return (0);
