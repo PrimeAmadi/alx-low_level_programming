b64bc15dedd5eda3745729230214a8452cefcc3b
Prime <pchikweru@gmail.com>
2023-02-26 17:02:40 -0800

    using gitacp

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,6 +9,9 @@ void more_numbers(void)
 {
 	int i;
 	int d;
+	char str[2];
+
+	str[0] = '1';
 
 	for (i = 0; i < 10; i++)
 	{
@@ -18,8 +21,9 @@ void more_numbers(void)
 				putchar('0' + d);
 			if (d >= 10)
 			{
-				putchar('1');
-				putchar('0' + d - 10);
+				str[1] = '0' + d - 10;
+				for (int si = 0; si < 2; si++)
+					putchar(str[si]);
 			}
 		}
 		putchar('\n');
