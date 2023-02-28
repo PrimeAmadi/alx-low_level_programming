742749910b84e2bd1fa20a5f5c155b62f68542d5
Prime <pchikweru@gmail.com>
2023-02-27 17:58:48 -0800

    using gitacp

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s : pointer address to the string to be
+ * evaluated
+ * Return: int (length of the string)
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s || *s)
+		return (0);
+
+	while (s[len++])
+		;
+
+	return (--len);
+}
