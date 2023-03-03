19647d1c35844bf6eaf0d4425a3df37c5680541c
Prime <pchikweru@gmail.com>
2023-03-02 21:57:22 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,43 @@
 #include "main.h"
 
 /**
+ * _strncat - concatenate  n char of src to the
+ *  the end of dest string and return
+ *  the resultant string (which is dest).
+ *
+ * @src: string to be concatinated to back dest
+ * @dest: string to be concatenated to
+ * @n: the number of char from srcs to be cat to
+ * dest
+ *
+ * Return: @dest pointer to the updated string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	unsigned long d = 0;
+	unsigned long s = 0;
+
+	if (dest == NULL)
+		return (NULL);
+	if (src == NULL || (n < 0))
+		return (dest);
+
+	while (dest[d])
+		d++;
+/*
+ * d is now the number of element in
+ * the strng including '\0' terminator.
+ * counting from zero
+ */
+
+	while (src[s] && (n > 0))
+	{
+		dest[d + s] = src[s];
+		s++;
+		n--;
+	}
+	dest[d + s] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/run.c
+++ 0x06-pointers_arrays_strings/run.c
@@ -3,6 +3,6 @@
 int main(void)
 {
 	char str[20] = "abcdef";
-	printf("%s\n\n", _strcat(str, "123456"));
+	printf("%s\n\n", _strncat(str, "123456", 3));
 	return (0);
 }
