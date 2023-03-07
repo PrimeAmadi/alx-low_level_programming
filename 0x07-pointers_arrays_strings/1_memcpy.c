bfc9fd4e86b13440fa127ac6abc1f1386654dcbc
Prime <pchikweru@gmail.com>
2023-03-06 19:51:38 -0800

    task 1 memcpy simple impelementation

---
+++ 0x07-pointers_arrays_strings/1_memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from
+ * src to dest
+ * @src: bytes to copy to @dest
+ * @dest: where to copy bytes from @src
+ * @n: number of bytes to copy
+ * Return: @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
