0780275a78ed6116cb39b2f865be41eab51c1fda
Prime <pchikweru@gmail.com>
2023-03-06 18:22:18 -0800

    task 0 memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * fill mem s with b
+ * _memset - fill mem s with n number of b
  * @s: mem to fill
  * @b: char to fill with
+ * @n: number of char to fill
  * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
