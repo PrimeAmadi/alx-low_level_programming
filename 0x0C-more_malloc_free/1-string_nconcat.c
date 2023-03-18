db26319f65af54c8b0b6853587ff744ae330afb1
Prime <pchikweru@gmail.com>
2023-03-17 20:10:26 -0700

    task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -78,7 +78,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	_memncpy(result, s1, s1len);
-	_memncpy(result + s1len, s2, s2len);
+	_memncpy(result + s1len, s2, n);
 
 	return (result);
 }
