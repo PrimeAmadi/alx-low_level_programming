46f3ff24db4f601b4a60d90bb932fbaf73275508
Prime <pchikweru@gmail.com>
2023-03-20 21:44:24 -0700

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog profile
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+
+#endif /* DOG_H */
