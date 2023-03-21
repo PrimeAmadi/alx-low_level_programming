db70b960be16e626d73c303972b64c4b75e137e5
Prime <pchikweru@gmail.com>
2023-03-21 09:23:52 -0700

    task 0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog struct
+ * @d: pointer to struct dog
+ * @name: map to name of dog
+ * @age: map to age of dog
+ * @owner: map to owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
