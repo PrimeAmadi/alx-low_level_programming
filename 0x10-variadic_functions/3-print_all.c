6041c1890ac582847247ce5ad1bd81acce9b5f7a
Prime <pchikweru@gmail.com>
2023-03-24 19:42:57 -0700

    update to task 3, change (nil) to nil

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -48,7 +48,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
-					str = "(nil)";
+					str = "nil";
 				printf("%s", str);
 				break;
 			default:
