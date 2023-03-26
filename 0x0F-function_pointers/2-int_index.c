e8257dc46446f161d589ede6c98575ddc505c0b0
Prime <pchikweru@gmail.com>
2023-03-25 19:18:55 -0700

    correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,11 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (!array || !cmp)
+		return (-1);
+
 	if (size <= 0)
 		return (-1);
 
-	for(;i < size;i++)
-		if((*cmp)(array[i]) != 0)
+	for (; i < size; i++)
+		if ((*cmp)(array[i]) != 0)
 			return (i);
 
 	return (-1);
