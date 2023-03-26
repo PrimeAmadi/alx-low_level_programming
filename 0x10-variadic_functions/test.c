2b37c87330381a40f4cda89564bcb045bc024499
Prime <pchikweru@gmail.com>
2023-03-25 20:39:14 -0700

    correction

---
+++ 0x10-variadic_functions/test.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("cseis", 'B', 0, 5, "stSchool");
+    return (0);
+}
