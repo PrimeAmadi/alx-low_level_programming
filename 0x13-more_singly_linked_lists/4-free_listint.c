ea71928b7036e60ece4b09b7dabd66695dfe6f20
Prime <pchikweru@gmail.com>
2023-04-07 16:52:47 -0700

    task 4 done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include  "lists.h"
+
+/**
+ * free_listint - free all memory
+ * allocated for listint list
+ * @head: head node of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+
+	free_listint(head->next);
+	free(head);
+}
