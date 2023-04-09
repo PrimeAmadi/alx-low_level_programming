f01181f59660c9d1c2e49cbc4d4dcde313bb6890
Prime <pchikweru@gmail.com>
2023-04-08 22:11:58 -0700

    task 5 done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees up
+ * listint_t elements in memory
+ * and sets head node to NULL
+ * @head: node pass by reference
+ * from which to free the list
+ * still the end.
+ */
+
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *cursor = NULL;
+	listint_t *cursor_temp = NULL;
+
+	if (!head && !(*head))
+		return;
+
+	cursor = *head;
+
+	while (cursor)
+	{
+		cursor_temp = cursor;
+		cursor = cursor->next;
+		free(cursor_temp);
+
+	}
+
+	*head = NULL;
+
+}
